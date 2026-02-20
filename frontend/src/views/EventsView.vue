076600c1ccd2768c7f8ea711f60411ac5cb8aafb
Ayan Hussain <23f1000932@ds.study.iitm.ac.in>
2026-02-20 02:33:32 -0800

    feat: Add EventsView to display all events

---
+++ frontend/src/views/EventsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="events-view">
+    <h1>All Events</h1>
+    <EventList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import EventList from '../components/EventList.vue';
+</script>
+
+<style scoped>
+.events-view {
+  padding: 2rem;
+}
+
+.events-view h1 {
+  margin-bottom: 2rem;
+  color: #333;
+}
+</style>
