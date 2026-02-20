73bc28dfa2e28211bf39d7a2f7e77cb674d3063f
Ayan Hussain <23f1000932@ds.study.iitm.ac.in>
2026-02-20 05:55:25 -0800

    Fix router imports and add admin/organizer routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import EventsView from '../views/EventsView.vue';
-import Login from '../views/Login.vue/Login.vue'
-import Register from '../views/Register.vue/Register.vue'
+import LoginView from '../views/LoginView.vue';
+import RegisterView from '../views/RegisterView.vue';
+import AdminDashboard from '../views/AdminDashboard.vue';
+import OrganizerDashboard from '../views/OrganizerDashboard.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -20,18 +22,27 @@ const routes: Array<RouteRecordRaw> = [
     name: 'EventDetail',
     component: () => import('../views/EventDetail.vue'),
   },
-    {
+  {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: LoginView
   },
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: RegisterView
   },
-  ];
-  
+  {
+    path: '/admin',
+    name: 'AdminDashboard',
+    component: AdminDashboard
+  },
+  {
+    path: '/organizer',
+    name: 'OrganizerDashboard',
+    component: OrganizerDashboard
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
